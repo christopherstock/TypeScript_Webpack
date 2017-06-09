5247ee2771bd988fe176962bcc6489b76a51c9b0
Christopher Stock <email@christopherstock.de>
2017-06-09 17:19:42 +0200

    Added node typescript initial setup.

---
+++ setup.bat
@@ -0,0 +1,16 @@
+
+: create new nodejs project
+npm init -y
+
+: install typescript compiler
+npm install typescript --save-dev 
+
+: install nodejs typescript definitions
+npm install @types/node --save-dev 
+
+: create tsconfig.json compiler option file
+node ./node_modules/typescript/lib/tsc --init 
+
+
+
+
