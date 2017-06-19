046192852fb6a7d081db52f74cbdabcc953dbc2d
Christopher Stock <email@christopherstock.de>
2017-06-19 15:34:30 +0200

    Completed webpack demo. Added webpack config file.

--- setup_webpack.bat
+++ setup_webpack.bat
@@ -9,7 +9,10 @@ npm install --save lodash
 
 
 : run webpack module bundler ===========================================================================================
-
 .\node_modules\.bin\webpack .\src\index.js .\dist\bundle.js
 
+: run webpack module bundler using alternate webpack config file =======================================================
+.\node_modules\.bin\webpack --config webpack.config.js
 
+: run webpack module bundler via npm after adding "build": "webpack" to the 'scripts' section of package.json ==========
+npm run build
