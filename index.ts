4b9c1f9939892d6384bc3e45ffc5dc42add62af7
Christopher Stock <email@christopherstock.de>
2017-06-19 10:51:55 +0200

    Added 'live compile and run' mechanism via 'ts-node' and 'nodemon'.

---
+++ index.ts
@@ -0,0 +1,18 @@
+
+    // impossible via node ?
+    //window.alert("The going is much more than trying from this point!");
+
+    console.log("index.ts being invoked");
+
+    class NodeTest1
+    {
+        public static test()
+        {
+            console.log("NodeTest1::test() being invoked");
+
+
+
+        }
+    }
+
+    NodeTest1.test();
--- setup.bat
+++ setup.bat
@@ -1,16 +1,36 @@
 
-: create new nodejs project
+: initial setup ========================================================================================================
+
+: create new nodejs project - creates package.json (node package config)
 npm init -y
 
-: install typescript compiler
-npm install typescript --save-dev 
+: install 'typescript compiler'
+npm install typescript --save-dev
 
-: install nodejs typescript definitions
-npm install @types/node --save-dev 
+: install 'nodejs typescript definitions'
+npm install @types/node --save-dev
 
-: create tsconfig.json compiler option file
-node ./node_modules/typescript/lib/tsc --init 
+: create new typescript project - creates tsconfig.json (compiler option file)
+node ./node_modules/typescript/lib/tsc --init
 
+
+: live compile & run ===================================================================================================
+
+: add 'ts-node' for live compile & run in node
+npm install ts-node --save-dev
+
+: add nodemon that invokes 'ts-node'
+npm install nodemon --save-dev
+
+
+: setup 'tsconfig.json' and 'package.json' for auto compilation ============================================================
+
+: add "typeRoots" with value "node_modules/@types" to the TypeScript compiler options!
+
+: add the 'scripts' tag to 'package.json'
+
+: start npm and watch live compiling of specified TypeScript file
+npm start
 
 
 
