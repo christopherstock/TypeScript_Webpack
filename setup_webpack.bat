ba5d038ab2bc04ee05bf2ce27d0d644ce82e86fd
Christopher Stock <email@christopherstock.de>
2017-06-19 14:35:20 +0200

    Added 'webpack' npm module. Successfully completed 1st webpack build structure.

---
+++ setup_webpack.bat
@@ -0,0 +1,15 @@
+
+: initial setup ========================================================================================================
+
+: install webpack
+npm install --save-dev webpack
+
+: install the 'lodsash' library locally
+npm install --save lodash
+
+
+: run webpack module bundler ===========================================================================================
+
+.\node_modules\.bin\webpack .\src\index.js .\dist\bundle.js
+
+
